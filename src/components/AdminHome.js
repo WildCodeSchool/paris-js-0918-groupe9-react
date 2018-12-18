1f4da9d2fea0f964b2d7653251a7b83551880157
Halim <aktashalmim.pro@gmail.com>
2018-12-18 18:02:14 +0100

    clubs pages creation

--- src/components/AdminHome.js
+++ src/components/AdminHome.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { getToken, getClubId } from '../helper/tokenHelper';
-import '../CSS/Adminhome.css';
 import AdminHeader from './AdminHeader';
 import '../CSS/AdminHome.css'
 
--- src/components/AdminTousClubs.js
+++ src/components/AdminTousClubs.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { getToken, getClubId } from '../helper/tokenHelper';
-
+import '..//CSS/AdminTousClubs.css'
 import AdminHeader from './AdminHeader';
 import '../CSS/AdminHome.css'
 
@@ -14,7 +14,7 @@ class AdminTousClubs extends Component {
     }
 
     componentDidMount() {
-        const url = "http://localhost:3030/user"
+        const url = "http://localhost:3030/club"
         axios({
             method: "GET",
             url: url,
@@ -39,12 +39,30 @@ class AdminTousClubs extends Component {
             return (
                 <div>
                     <AdminHeader />
-                    <div className="groupe-input">
-                        <Link to="/"><button>CLUB</button></Link>    <br />
-
+                    <div className="allcards">
+                        <div className="custom-select1">
+                            <h1>Nom</h1>
+                            {this.state.user.map((e,i) => 
+                                <p key={i}>{e.email}</p>
+                            )}
+                        </div>
+                        <div className="custom-select2">
+                            <h1>Convention Signée</h1>
+                        </div>
+                        <div className="custom-select3">
+                            <h1>Logo</h1>
+                        </div>
+                        <div className="custom-select4">
+                            <h1>Commande</h1>
+                        </div>
+                        <div className="custom-select5">
+                            <h1>Formulaire Satisfaction</h1>
+                        </div>
+                        <div className="custom-select6">
+                            <h1>Action</h1>
+                        </div>
                     </div>
                 </div>
-
             )
         } else {
             return (<div>Loading...</div>);
@@ -53,3 +71,33 @@ class AdminTousClubs extends Component {
     }
 }
 export default AdminTousClubs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div class="custom-select">
+                            <div class="custom">
+                                <h1>Nom</h1>
+                                <select>
+                                    <option value="0">Nom</option>
+                                    <option value="1">Récent</option>
+                                    <option value="2">Ancien</option>
+                                </select>
+                            </div>
+                        </div> */}
