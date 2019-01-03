0b981a277e1520e6090847fc1888d20f9aec0cbc
Hugo Hung <lnhugo@hotmail.fr>
2019-01-03 10:15:17 +0100

    add filter club

--- src/components/AdminTousClubs.js
+++ src/components/AdminTousClubs.js
@@ -1,120 +1,126 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { getToken, getClubId } from '../helper/tokenHelper';
-import '..//CSS/AdminTousClubs.css'
-import AdminHeader from './AdminHeader';
-import '../CSS/AdminHome.css'
+import React, { Component } from "react";
+import axios from "axios";
+import { getToken } from "../helper/tokenHelper";
+import "../CSS/AdminTousClubs.css";
+import AdminHeader from "./AdminHeader";
 
 class AdminTousClubs extends Component {
-    state = {
-        open: true,
-        isLoaded: false,
-        user: []
-    }
+  state = {
+    open: true,
+    isLoaded: false,
+    user: []
+  };
 
-    componentDidMount() {
-        const url = "http://localhost:3030/club"
-        axios({
-            method: "GET",
-            url: url,
-            headers: getToken()
-        })
-            .then(
-                (result) => {
-                    // console.log(result.data)
-                    this.setState({
-                        isLoaded: true,
-                        user: result.data
+  componentDidMount() {
+    const url = "http://localhost:3030/club";
+    axios({
+      method: "GET",
+      url: url,
+      headers: getToken()
+    }).then(
+      result => {
+        // console.log(result.data)
+        this.setState({
+          isLoaded: true,
+          user: result.data
+        });
+      },
+      error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
 
-                    });
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error,
-                    });
-                })
-    }
-    render() {
-        if (this.state.isLoaded) {
-            return (
-                <div>
-                    <AdminHeader />
-                    <div className="allcards">
-                        <div className="custom-select1">
-                            <h1>Nom</h1>
-                            {this.state.user.map((e,i) => 
-                                <p key={i}>{e.clubName}</p>
-                            )}
-                        </div>
-                        <div className="custom-select2">
-                            <h1>Convention Signée</h1>
-                            {this.state.user.map((e,i) => 
-                                <p key={i}>{e.contractName}</p>
-                            )}
-                        </div>
-                        <div className="custom-select3">
-                            <h1>Logo</h1>
-                            {this.state.user.map((e,i) => 
-                                <img className="img-logo" key={i} src={e.url_logo} />
-                            )}
-                        </div>
-                        <div className="custom-select4">
-                            <h1>Commande</h1>
-                            {this.state.user.map((e,i) => 
-                                <p key={i}>{e.status}</p>
-                            )}
-                        </div>
-                        <div className="custom-select5">
-                            <h1>Formulaire Satisfaction</h1>
-                            {this.state.user.map((e,i) => 
-                                <p key={i}>{e.surveyStatus}</p>
-                            )}
-                        </div>
-                        <div className="custom-select6">
-                            <h1>Action</h1>
-                            {this.state.user.map((e,i) => 
-                                <p key={i}>{e.actionName}</p>
-                            )}
-                        </div>
-                    </div>
-                </div>
-            )
-        } else {
-            return (<div>Loading...</div>);
+  filter = e => {
+      const url = `http://localhost:3030/club/${e.target.value}`;
+      axios({
+        method: "GET",
+        url: url,
+        headers: getToken()
+      }).then(
+        result => {
+          // console.log(result.data)
+          this.setState({
+            isLoaded: true,
+            user: result.data
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
         }
+      );
+  };
 
+  render() {
+    if (this.state.isLoaded) {
+      return (
+        <div>
+          <AdminHeader />
+          <div className="allcards">
+            <div className="custom-select1">
+              <div className="type">
+                <h1>Nom</h1>
+                <select name="filter" onClick={this.filter}>
+                  <option value="filtername">Nom</option>
+                  <option value="filterdate">Mise à jour</option>
+                </select>
+              </div>
+              {this.state.user.map((e, i) => (
+                <p className="element" key={i}>{e.clubName}</p>
+              ))}
+            </div>
+            <div className="custom-select2">
+              <div className="type">
+                <h1>Convention Signée</h1>
+              </div>
+              {this.state.user.map((e, i) => (
+                <p className="element" key={i}>{e.contractName}</p>
+              ))}
+            </div>
+            <div className="custom-select3">
+              <div className="type">
+                <h1>Logo</h1>
+              </div>
+              {this.state.user.map((e, i) => (
+               <p className="element"><img key={i} className="img-logo" src={e.url_logo} alt="logo" /></p>
+              ))}
+            </div>
+            <div className="custom-select4">
+              <div className="type">
+                <h1>Commande</h1>
+              </div>
+              {this.state.user.map((e, i) => (
+                <p className="element" key={i}>{e.status}</p>
+              ))}
+            </div>
+            <div className="custom-select5">
+              <div className="type">
+                <h1>Formulaire Satisfaction</h1>
+              </div>
+              {this.state.user.map((e, i) => (
+                <p className="element" key={i}>{e.surveyStatus}</p>
+              ))}
+            </div>
+            <div className="custom-select6">
+              <div className="type">
+                <h1>Action</h1>
+              </div>
+              {this.state.user.map((e, i) => (
+                <p className="element" key={i}>{e.actionName}</p>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <div>Loading...</div>;
     }
+  }
 }
 export default AdminTousClubs;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <div class="custom-select">
-                            <div class="custom">
-                                <h1>Nom</h1>
-                                <select>
-                                    <option value="0">Nom</option>
-                                    <option value="1">Récent</option>
-                                    <option value="2">Ancien</option>
-                                </select>
-                            </div>
-                        </div> */}
