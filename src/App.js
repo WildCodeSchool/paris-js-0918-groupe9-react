18d158ab0fe4399100dafda7c2be2df1e3a8853a
Hugo Hung <lnhugo@hotmail.fr>
2019-01-09 16:46:15 +0100

    rendu bon de commande

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
-import React, { Component } from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 
-import LoginClub from './components/LoginClub';
-import ClubHome from './components/ClubHome';
-import AdminSponsore from './components/AdminSponsore';
-import LoginAdmin from './components/LoginAdmin';
-import AdminHome from './components/AdminHome';
-import AdminClub from './components/AdminClub';
-import AdminCreationEspace from './components/AdminCreationEspace';
-import AjouteProjet_Club from './components/AjouteProjet_Club';
-import AdminProject from './components/AdminProject';
-import CreationProjetGlobal from './components/CreationProjetGlobal';
-import AdminParameters from './components/AdminParameters';
+import LoginClub from "./components/LoginClub";
+import ClubHome from "./components/ClubHome";
+import AdminSponsore from "./components/AdminSponsore";
+import LoginAdmin from "./components/LoginAdmin";
+import AdminHome from "./components/AdminHome";
+import AdminClub from "./components/AdminClub";
+import AdminCreationEspace from "./components/AdminCreationEspace";
+import AjouteProjet_Club from "./components/AjouteProjet_Club";
+import AdminProject from "./components/AdminProject";
+import CreationProjetGlobal from "./components/CreationProjetGlobal";
+import AdminParameters from "./components/AdminParameters";
+import Order_render from "./components/Order_render";
 
-import ClubConvention from './components/ClubConvention';
+import ClubConvention from "./components/ClubConvention";
 
-
-import AdminTousClubs from './components/AdminTousClubs';  
-import AdminGestionProjet from'./components/AdminGestionProjet';
-import LIsteProduits from './components/LIsteProduits';
-
+import AdminTousClubs from "./components/AdminTousClubs";
+import AdminGestionProjet from "./components/AdminGestionProjet";
+import LIsteProduits from "./components/LIsteProduits";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-                <Switch>
-                  <Route exact path="/" component={LoginClub} />
-                  <Route path="/admin" component={LoginAdmin} />
-                  <Route path="/adminhome" component={AdminHome} />
-                  <Route path="/clubhome" component={ClubHome} />
-
-                  <Route path="/ClubConvention" component={ClubConvention} />
-
-                  <Route path="/admin-sponsor" component={AdminSponsore} />
-                  <Route path="/admin-club/:id" component={AdminClub} />
-                  <Route path="/admin-creation-espace" component={AdminCreationEspace} />
-                  <Route path="/ajoute-projet-club" component={AjouteProjet_Club}/>
-                  <Route path="/admin-creation-projetglobal" component={CreationProjetGlobal}/>
-                  <Route path="/admin-project/:id" component={AdminProject}/>
-                  <Route path="/admin-parameters" component={AdminParameters} />
-                  <Route path="/admin-tous-clubs" component={AdminTousClubs}/>
-                  <Route path="/admin-gestion-projet" component={AdminGestionProjet}/>
-
-                  <Route path="/liste-produits" component={LIsteProduits}/>                  
-                  
-
-                </Switch>
-            </BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={LoginClub} />
+            <Route path="/admin" component={LoginAdmin} />
+            <Route path="/adminhome" component={AdminHome} />
+            <Route path="/clubhome" component={ClubHome} />
+            <Route path="/ClubConvention" component={ClubConvention} />
+            <Route path="/admin-sponsor" component={AdminSponsore} />
+            <Route path="/admin-club/:id" component={AdminClub} />
+            <Route
+              path="/admin-creation-espace"
+              component={AdminCreationEspace}
+            />
+            <Route path="/ajoute-projet-club" component={AjouteProjet_Club} />
+            <Route
+              path="/admin-creation-projetglobal"
+              component={CreationProjetGlobal}
+            />
+            <Route path="/admin-project/:id" component={AdminProject} />
+            <Route path="/admin-parameters" component={AdminParameters} />
+            <Route path="/admin-tous-clubs" component={AdminTousClubs} />
+            <Route
+              path="/admin-gestion-projet"
+              component={AdminGestionProjet}
+            />
+            <Route path="/liste-produits" component={LIsteProduits} />
+            <Route path="/order-render/:id" component={Order_render}/>
+          </Switch>
+        </BrowserRouter>
         {/* <GenericModelCRUD
           mappings={mappings} model={model} table={table}
         /> */}
--- src/components/AjouteProjet_Club.js
+++ src/components/AjouteProjet_Club.js
@@ -1,188 +1,208 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import {Redirect } from 'react-router-dom';
-import queryString from 'query-string';
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import queryString from "query-string";
 
-import { getToken, getClubId } from '../helper/tokenHelper';
-import AdminHeader from './AdminHeader';
-import '../CSS/AjouteProjet_Club.scss'
+import { getToken, getClubId } from "../helper/tokenHelper";
+import AdminHeader from "./AdminHeader";
+import "../CSS/AjouteProjet_Club.scss";
 
 class AjouteProjet_Club extends Component {
-    state = {
-        isLoaded: false,
-        error: null,
-        projets: [],
-        clubs: [],
-        project_id: undefined,
-        club_id: undefined,
-        name: "",
-        url_contract: "",
-        file: undefined,
-    }
-    
-    componentDidMount() {
-        const values = queryString.parse(this.props.location.search);
-        fetch("http://localhost:3030/project/")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    const queryProjetId = values.projetid  ? values.projetid  :result[0].id
-                    this.state.projets = result;
-                    this.state.project_id = queryProjetId ;
-                    return fetch("http://localhost:3030/club");
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error,
-                    });
-                })
-            .then(res => res.json())
-            .then(
-                (result) => {
-                   
-                    const queryClubId = values.clubid ? values.clubid : result[0].id;
-                    this.setState({
-                        isLoaded: true,
-                        clubs: result,
-                        club_id:queryClubId
-                    });
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error,
-                    });
-                })
-    }
+  state = {
+    isLoaded: false,
+    error: null,
+    projets: [],
+    clubs: [],
+    project_id: undefined,
+    club_id: undefined,
+    name: "",
+    url_contract: "",
+    file: undefined
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const {project_id, name, url_contract,club_id} = this.state;
-        const body = {
-            project_id,
-            club_id,
-            name,
-            url_contract,
-        };
+  componentDidMount() {
+    const values = queryString.parse(this.props.location.search);
+    fetch("http://localhost:3030/project/")
+      .then(res => res.json())
+      .then(
+        result => {
+          const queryProjetId = values.projetid
+            ? values.projetid
+            : result[0].id;
+          this.state.projets = result;
+          this.state.project_id = queryProjetId;
+          return fetch("http://localhost:3030/club");
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+      .then(res => res.json())
+      .then(
+        result => {
+          const queryClubId = values.clubid ? values.clubid : result[0].id;
+          this.setState({
+            isLoaded: true,
+            clubs: result,
+            club_id: queryClubId
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
 
-        axios.post("http://localhost:3030/contract/ajouteclub", body)
-            .then((res) => {
-                if (res.status === 200) {
-                    alert("Contrat est créé");
-                    this.props.history.push(`/admin-project/${project_id}`)
-                }
-            }
-            )
-            .catch(function (error) {
-                console.log(error);
-            })
-
-    }
-    handleChange = (event) => {
-        const key = event.target.name;
-        const value = event.target.value;
-        this.setState({ [key]: value });
-    }
-
-    onChangeFile = e => {
-        this.setState({ file: e.target.files[0] });
+  handleSubmit = e => {
+    e.preventDefault();
+    const { project_id, name, url_contract, club_id } = this.state;
+    const body = {
+      project_id,
+      club_id,
+      name,
+      url_contract
     };
 
-    handleUpload = (e) => {
-        const key  = e.target.name;
-        const formdata = new FormData()
-        formdata.append('file', this.state.file)
-        axios({
-            method: 'post',
-            url: 'http://localhost:3030/contract/uploaddufichier',
-            data: formdata,
-            config: { headers: { 'Content-Type': 'multipart/form-data' } }
-        })
-            .then((res) => {
-                if (res.status === 200) {
-                    alert("Fichiers uploadé");
-                    this.setState({
-                        [key] : res.data
-                    })
-                }
-            }
-            )
-            .catch(function (error) {
-                console.log(error);
-            })
+    axios
+      .post("http://localhost:3030/contract/ajouteclub", body)
+      .then(res => {
+        if (res.status === 200) {
+          alert("Contrat est créé");
+          this.props.history.push(`/admin-project/${project_id}`);
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+  handleChange = event => {
+    const key = event.target.name;
+    const value = event.target.value;
+    this.setState({ [key]: value });
+  };
 
+  onChangeFile = e => {
+    this.setState({ file: e.target.files[0] });
+  };
+
+  handleUpload = e => {
+    const key = e.target.name;
+    const formdata = new FormData();
+    formdata.append("file", this.state.file);
+    axios({
+      method: "post",
+      url: "http://localhost:3030/contract/uploaddufichier",
+      data: formdata,
+      config: { headers: { "Content-Type": "multipart/form-data" } }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          alert("Fichiers uploadé");
+          this.setState({
+            [key]: res.data
+          });
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { error, isLoaded, projets, clubs, project_id, club_id } = this.state;
+    const values = queryString.parse(this.props.location.search);
+    const disabledClub = values.clubid !== undefined;
+    const disabledProject = values.projetid !== undefined;
+    if (error) {
+      return <div>Error:{error.message}</div>;
     }
-    
-    render() {
-        const { error, isLoaded, projets, clubs, project_id,club_id} = this.state;
-        const values = queryString.parse(this.props.location.search);
-        const disabledClub = values.clubid !== undefined;
-        const disabledProject = values.projetid !== undefined;
-        if (error) {
-            return (
-                <div>
-                    Error:{error.message}
-                </div>
-            );
-        }
-        if (!isLoaded) {
-            return <div> Loading... </div>;
-        }
-        if ( projets.length !== 0 && clubs.length !== 0) {
-            return (
-                <div className="ajoute-projet-club">
-                    <div>
-                        <AdminHeader />
-                    </div>
-                    <p>Ajouter un projet au club</p>
-                    <form className="formulaire" onSubmit={this.handleSubmit}>
-                        <label>
-                            Sélectioner un club :
-                                <select disabled={disabledClub} name="club_id" value={club_id} onChange={this.handleChange}>
-                                {clubs.map(club =>
-                                    <option value={club.id}>{club.name}</option>
-                                )}
-                            </select>
-                        </label>
-                        <br />
-                        <label>
-                            Sélectioner un projet global:
-                                <select disabled={disabledProject} name="project_id" value={project_id} onChange={this.handleChange}>
-                                {projets.map(projet =>
-                                    <option value={projet.id}>{projet.name}</option>
-                                )}
-                            </select>
-                        </label>
-                        <br />
-                        <label>
-                            Nom de contrat (convention):
-                            <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
-                        </label>
-                        <br />
-                        <label>
-                            bon de commande :
-                            <input name="" type="text"  onChange={this.handleChange} />
-                        </label>
-                        <br />
-                        <button type="submit" value="Submit"> Créer un nouveau contrat-club </button>
-                    </form>
-                    <div className="formulaire">
-                    <label>
-                        convention :
-                        <input type="file" name="file" accept=".pdf" onChange={this.onChangeFile} /> <br />
-                        <button name="url_contract" onClick={this.handleUpload}>Upload</button>
-                        
-                    </label>
-                    </div>
-                    
-                </div>
-            )
-        }
-        return (
-            <Redirect to='/admin-creation-espace' />
-        )
-        
+    if (!isLoaded) {
+      return <div> Loading... </div>;
     }
+    if (projets.length !== 0 && clubs.length !== 0) {
+      return (
+        <div className="ajoute-projet-club">
+          <div>
+            <AdminHeader />
+          </div>
+          <p>Ajouter un projet au club</p>
+          <form className="formulaire" onSubmit={this.handleSubmit}>
+            <label>
+              Sélectioner un club :
+              <select
+                disabled={disabledClub}
+                name="club_id"
+                value={club_id}
+                onChange={this.handleChange}
+              >
+                {clubs.map(club => (
+                  <option value={club.id}>{club.name}</option>
+                ))}
+              </select>
+            </label>
+            <br />
+            <label>
+              Sélectioner un projet global:
+              <select
+                disabled={disabledProject}
+                name="project_id"
+                value={project_id}
+                onChange={this.handleChange}
+              >
+                {projets.map(projet => (
+                  <option value={projet.id}>{projet.name}</option>
+                ))}
+              </select>
+            </label>
+            <br />
+            <label>
+              Nom de contrat (convention):
+              <input
+                name="name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <br />
+            <button type="submit" value="Submit">
+              {" "}
+              Créer un nouveau contrat-club{" "}
+            </button>
+          </form>
+          <div className="formulaire">
+            <label>
+              convention :
+              <input
+                type="file"
+                name="file"
+                accept=".pdf"
+                onChange={this.onChangeFile}
+              />{" "}
+              <br />
+              <button name="url_contract" onClick={this.handleUpload}>
+                Upload
+              </button>
+            </label>
+            <br />
+            <label>
+              bon de commande :
+              <form>
+                  
+              </form>
+            </label>
+          </div>
+        </div>
+      );
+    }
+    return <Redirect to="/admin-creation-espace" />;
+  }
 }
-export default AjouteProjet_Club; 
+export default AjouteProjet_Club;
